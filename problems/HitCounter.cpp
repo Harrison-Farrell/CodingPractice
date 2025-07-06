4feb46d5db0a8ca3c7189546fa9f63860b81e9e0
Harrison Farrell <h.thomas.farrell@gmail.com>
2025-07-06 15:46:27 +1000

    Create HitCounter Class
    creating the initial HitCounter Class

---
+++ problems/HitCounter.cpp
@@ -0,0 +1,47 @@
+#include <unordered_map>
+
+class HitCounter {
+  // Use an unordered map to store the count of hits for each timestamp
+  private:
+    std::unordered_map<int, int> hitCounts;
+
+  public:
+    HitCounter(){};
+
+  void hit(int timestamp) {
+    hitCounts[timestamp]++;
+  }
+
+  int getHits(int timestamp)
+  {
+    int hits = 0;
+
+    for (auto& entry : hitCounts) {
+      if (timestamp - entry.first < 300) {
+        hits += entry.second;
+      }
+    }
+    return hits;
+  }
+};
+
+int main(int argc, char** argv)
+{
+  HitCounter* counter = new HitCounter();
+  counter->hit(1);
+  counter->hit(3);
+
+  int hits4 = counter->getHits(4); // 2
+
+  counter->hit(6);
+  counter->hit(6); // multiple  hits for the same time
+  counter->hit(7);
+
+  int hits7 = counter->getHits(7); // 4, time 1 is out of context now
+  int hits10 = counter->getHits(10); // 3 time 3 is out of context now
+  int hits15 = counter->getHits(15); // 0 all hits are out of context now
+
+  return 0;
+
+}
+

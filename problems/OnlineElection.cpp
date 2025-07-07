9d3baa30aade19f98b7a5dc23056b51b84c70a3f
Harrison Farrell <h.thomas.farrell@gmail.com>
2025-07-07 16:17:23 +1000

    OnlineElection
    Starting to work on the online election problem

---
+++ problems/OnlineElection.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+
+// An underordered_map of <person, 
+
+
+
+class TopVotedCandiate {
+  // constructor accepting an array of persons and the timestamps
+  TopVotedCandiate(int[] persons, int[] times)
+  {
+
+  }
+
+  // Returns the number of the person that was leading the election at time t
+  int q(int t)
+}
